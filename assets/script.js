378ecd4da4e9ebcd119d5f22d2bff0a5d68fb3a7
Woogster <john.m.wooley@gmail.com>
2021-11-15 00:31:28 -0600

    Added blank functions to code later

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,24 @@
 var displayCurrentTime = function() {
-    $("#currentDay").append(moment().format("[Today's Date is] dddd, MMMM Do YYYY, h:mm:ss a"));
+    $("#currentDay").append(moment().format("[Today's Date is] dddd, MMMM Do YYYY"));
+}
+
+var highlightTime = function() {
+   
+    if (moment().format(HH)==) {
+
+    }
+}
+
+var createScheduledTask = function() {
+
+}
+
+var saveTasks = function() {
+
+}
+
+var loadTasks = function() {
+
 }
 
 displayCurrentTime();
